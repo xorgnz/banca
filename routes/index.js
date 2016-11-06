cac3f3658400312719036470806731bf650620bd
Trond Nilsen <xorgnz@gmail.com>
2016-11-06 14:18:56 -0800

    Converted view engine to nunjucks Configured for use with nodemon www -> www.js Add placeholder favicon.ico

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('base.html', {
+        title: 'Express',
+        items: [
+            { name: "item1" },
+            { name: "item2" },
+            { name: "item3" },
+        ]
+    });
 });
 
 module.exports = router;
