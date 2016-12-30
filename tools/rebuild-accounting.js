bbc4f170f748a53118e6d68c1ed1b68c33415b4f
Trond <xorgnz@gmail.com>
2016-12-31 11:47:39 +1300

    Code improvement (no functionality change)

--- tools/rebuild-accounting.js
+++ tools/rebuild-accounting.js
@@ -5,16 +5,16 @@ const accountingDAO = require("../dao/accounting.js");
 Promise.resolve()
     .then(() => { return accountingDAO.listAll(db); })
     .then((accountings) => {
-        var promises = [];
-        for (var a of accountings)
-            promises.push(accountingDAO.calc(db, a.id));
-        return Promise.all(promises);
+        return Promise.resolve()
+            .then(() => {
+                var promises = [];
+                for (var a of accountings)
+                    promises.push(accountingDAO.calc(db, a.id));
+                return Promise.all(promises);
+            })
+            .then((accountings) => { return accountingDAO.cascade(accountings[0]); });
     })
-
-    .then(() => { return accountingDAO.listAll(db); })
-    .then((accountings) => { return accountingDAO.cascade(accountings[0]); })
     .then(() => { console.log("Rebuild complete!"); })
-
     .catch((e) => {
         console.log("Rebuild failed!");
         console.log(e);
