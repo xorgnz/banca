fab79a3e1d09a3a8e820ea3c0fee381a272268da
Trond <xorgnz@gmail.com>
2017-01-03 18:32:50 +1300

    Entry dates now format ISO 8601

--- public/js/uiUtils.js
+++ public/js/uiUtils.js
@@ -1,5 +1,18 @@
-exports.formatDateIso8601 = function (date) {
-    var d = new Date(date);
-    return pad(d.getUTCFullYear(), 4) + "-" + pad(d.getUTCMonth(), 2) + "-" + pad(d.getUTCDate(), 2);
+var formatDateIso8601 = function (date) {
+
+    // Is a numeric date in string or number form
+    if (!isNaN(date)) {
+        return new Date(Number.parseInt(date)).toISOString().substr(0, 10);
+    }
+
+    // Is a string date
+    else if (typeof(date) === "string" && date.match(/(\d{4})-(\d{2})-(\d{2})/)) {
+        return new Date(date).toISOString().substr(0, 10);
+    }
+
+    // Is invalid
+    else {
+        return "Invalid";
+    }
 };
 
