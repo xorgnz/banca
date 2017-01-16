693021cded204bf0eecbb074008dde734ce24076
Trond <xorgnz@gmail.com>
2017-01-17 00:34:26 +1300

    UI uses accounting VO. Init now auto-creates all periods (saves creating them later)

--- public/js/objects/accounting.js
+++ public/js/objects/accounting.js
@@ -6,14 +6,16 @@ class Accounting extends AjaxRestObject {
         var self        = this;
 
         // Storage
-        this.id                = obj.id;
-        this.amount_start      = "" + obj.amount_start.toFixed(2);
-        this.amount_end        = "" + obj.amount_end.toFixed(2);
-        this.account_id        = obj.account_id;
-        this.period_id         = obj.period_id;
-        this.period_name       = obj.period_name;
-        this.period_date_start = obj.period_date_start;
-        this.period_date_end   = obj.period_date_end;
+        this.id           = obj.id;
+        this.amount_start = "" + obj.amount_start.toFixed(2);
+        this.amount_end   = "" + obj.amount_end.toFixed(2);
+        this.account_id   = obj.account_id;
+        this.period       = {
+            id:         obj.period.id,
+            name:       obj.period.name,
+            date_start: obj.period.date_start,
+            date_end:   obj.period.date_end
+        };
 
         // Register for AJAX calls
         // decorateAjaxRest(
@@ -39,15 +41,16 @@ class Accounting extends AjaxRestObject {
 
     expressAsTable() {
         var div = document.createElement("div");
+        div.appendChild(document.createTextNode(this));
         return div;
     }
 
     updateFromObject(obj) {
-        this.id = obj.id;
+        this.id           = obj.id;
         this.amount_start = obj.amount_start;
-        this.amount_end = obj.amount_end;
-        this.account_id = obj.account_id;
-        this.period_id = obj.period_id;
+        this.amount_end   = obj.amount_end;
+        this.account_id   = obj.account_id;
+        this.period_id    = obj.period_id;
     }
 
     valuesOnly() {
@@ -56,7 +59,7 @@ class Accounting extends AjaxRestObject {
             amount_start: this.amount_start,
             amount_end:   this.amount_end,
             account_id:   this.account_id,
-            period_id:    this.period_id,
+            period_id:    this.period.id,
         };
     }
 }
--- tools/init-db.js
+++ tools/init-db.js
@@ -1,11 +1,13 @@
 // Import modules
-var sqlite3 = require('sqlite3');
-var fs = require("fs");
+const sqlite3   = require('sqlite3');
+const fs        = require("fs");
+const logger    = require("../lib/debug.js").logger;
+const periodDAO = require("../dao/period.js");
 
 // Define constants
 const FILENAME_DB_LIVE = "database.sqlite";
 const FILENAME_DB_TEST = "test.sqlite";
-const FILENAME_SCHEMA = "schema.sql";
+const FILENAME_SCHEMA  = "schema.sql";
 
 // Show usage if requested
 function usage() {
@@ -22,16 +24,14 @@ console.log("About to initialize database file: " + filename);
 
 
 // Test that the database does not already exist
-if (fs.existsSync(filename))
-{
+if (fs.existsSync(filename)) {
     console.log("Cannot initialize database - a database file already exists.");
     console.log("If you want to replace it, you must either rename or manually delete it.");
     process.exit();
 }
 
 // Test that the schema exists
-if (!fs.existsSync(FILENAME_SCHEMA))
-{
+if (!fs.existsSync(FILENAME_SCHEMA)) {
     console.log("Cannot initialize database - schema file does not exist.");
     console.log("If you want to replace it, you must either rename or manually delete it.");
     process.exit();
@@ -40,17 +40,37 @@ if (!fs.existsSync(FILENAME_SCHEMA))
 // Connect to DB
 var db = new sqlite3.Database(filename);
 
-db.serialize(function () {
+// Load schema
+var schema  = fs.readFileSync(FILENAME_SCHEMA);
+var queries = schema.toString().split(';');
 
-    var schema = fs.readFileSync(FILENAME_SCHEMA);
-    var queries = schema.toString().split(';');
+// Deploy schema
+Promise.resolve()
+    .then(() => {
+        var promises = [];
+        for (var query of queries) {
+            if (query.trim().length > 0) {
+                promises.push(new Promise((resolve, reject) => {
+                    logger.trace("DB Initialization: " + query.trim());
+                    db.run(query, (err) => {
+                        if (err) {
+                            logger.error(err);
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                }));
+            }
+        }
 
-    for (var query of queries)
-    {
-        if (query.trim().length > 0)
-            db.run(query);
-    }
-});
+        return Promise.all(promises);
+    })
+
+    .then(() => { periodDAO.createOverDateRange(db, new Date("2000-01-01"), new Date("2020-12-31")); })
+    .then(() => { db.close(); })
+    .catch((err) => { console.log(err); });
+
 
 console.log("Database initialization complete");
 
