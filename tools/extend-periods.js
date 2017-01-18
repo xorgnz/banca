cc3c9de35442ae1e9d5cfa2cc10427b97fb82baf
Trond <xorgnz@gmail.com>
2017-01-19 01:13:39 +1300

    Accountings for all periods now created during database init and period extension. Includes cascade from previous accountings

--- tools/extend-periods.js
+++ tools/extend-periods.js
@@ -1,7 +1,11 @@
 // Import modules
-const sqlite3   = require('sqlite3');
-const logger    = require("../lib/debug.js").logger;
-const periodDAO = require("../dao/period.js");
+const sqlite3 = require('sqlite3');
+const logger  = require("../lib/debug.js").logger;
+const _       = require('lodash');
+
+const accountDAO    = require("../dao/account.js");
+const accountingDAO = require("../dao/accounting.js");
+const periodDAO     = require("../dao/period.js");
 
 // Define constants
 const FILENAME_DB_LIVE = "database.sqlite";
@@ -9,8 +13,8 @@ const FILENAME_DB_TEST = "test.sqlite";
 
 // Define requested date range
 // TODO - Get these from command line
-const DATE_START = "2000-01-01";
-const DATE_END = "2020-12-31";
+const DATE_START = "2015-01-01";
+const DATE_END   = "2018-12-31";
 
 // Show usage if requested
 function usage() {
@@ -26,8 +30,8 @@ var filename = process.argv[2] == "test" ? FILENAME_DB_TEST : FILENAME_DB_LIVE;
 console.log("Using database: " + filename);
 
 // Command Line - Select date range
-var date_start = new Date(process.argv[3]);
-var date_end = new Date(process.argv[4]);
+var date_start = new Date(process.argv[3] ? process.argv[3] : "2015-01-01");
+var date_end   = new Date(process.argv[4] ? process.argv[4] : "2018-12-31");
 if (isNaN(date_start.getTime()) || isNaN(date_end.getTime())) {
     logger.error("Cannot proceed - requested date range includes invalid dates:");
     logger.error("Parsed start date: " + date_start + " from " + process.argv[3]);
@@ -42,23 +46,52 @@ else {
 var db = new sqlite3.Database(filename);
 
 // Create periods
-
 Promise.resolve()
     .then(() => { return periodDAO.listAll(db); })
     .then((rows) => {
         var first = rows[0];
-        var last = rows[rows.length - 1];
+        var last  = rows[rows.length - 1];
 
         date_start = date_start.getTime() < first.date_start ? date_start : new Date(first.date_start);
-        date_end = date_end.getTime() > last.date_end ? date_end : new Date(date_end.date_end);
+        date_end   = date_end.getTime() > last.date_end ? date_end : new Date(last.date_end);
 
+        logger.trace("Creating periods over span " + date_start + " to " + date_end);
         return periodDAO.createOverDateRange(db, date_start, date_end);
     })
     .then(() => {
+        logger.trace("Extending accountings for existing accounts");
+        var periods  = null;
+        var accounts = null;
 
+        return Promise.resolve()
+            .then(() => { return periodDAO.listAll(db); })
+            .then((r) => { periods = r; })
+            .then(() => { return accountDAO.listAll(db); })
+            .then((r) => { accounts = r; })
+            .then(() => {
+                var period_ids = _.map(periods, "id");
+                var promises   = [];
+                for (var account of accounts) {
+                    promises.push(accountingDAO.createForPeriods(db, period_ids, account.id));
+                }
+                return Promise.all(promises);
+            })
     })
     .then(() => {
-        logger.trace("Periods now span " + date_start + " to " + date_end);
+        logger.trace("Cascading fresh accountings");
+        return Promise.resolve()
+            .then(() => { return accountDAO.listAll(db); })
+            .then((accounts) => {
+                var promises = [];
+                for (var account of accounts) {
+                    promises.push(Promise.resolve()
+                        .then(() => { return accountingDAO.listByAccount(db, account.id); })
+                        .then((accountings) => { return accountingDAO.cascade(db, accountings[0].id); }));
+                }
+                return Promise.all(promises);
+            });
+    })
+    .then(() => {
         db.close();
     })
     .catch((err) => { console.log(err); });
--- tools/init-db.js
+++ tools/init-db.js
@@ -3,8 +3,9 @@ const sqlite3 = require('sqlite3');
 const fs      = require("fs");
 const logger  = require("../lib/debug.js").logger;
 
-const accountDAO = require("../dao/account.js");
-const periodDAO  = require("../dao/period.js");
+const accountDAO    = require("../dao/account.js");
+const accountingDAO = require("../dao/accounting.js");
+const periodDAO     = require("../dao/period.js");
 
 // Define constants
 const FILENAME_DB_LIVE = "database.sqlite";
@@ -70,12 +71,16 @@ Promise.resolve()
     })
 
     .then(() => {
-        logger.trace("Creating initial periods from 2000 to 2020");
-        return periodDAO.createOverDateRange(db, new Date("2000-01-01"), new Date("2020-12-31"));
+        logger.trace("Creating initial periods from 2015 to 2017");
+        return periodDAO.createOverDateRange(db, new Date("2015-01-01"), new Date("2017-12-31"));
     })
     .then(() => {
         logger.trace("Creating initial account");
         return accountDAO.add(db, new accountDAO.Account(null, "First Account", "Your first account"));
+    })
+    .then((id) => {
+        logger.trace("Creating accountings for initial account");
+        return accountingDAO.createForAccount(db, id);
     })
     .then(() => {
         logger.trace("Database initialization complete");
